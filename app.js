0eb807c4ebda969584020b53468f1bce43f4d052
honour-dev <honouroke@gmail.com>
2023-05-07 21:13:48 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const date = (document.getElementById("date").innerHTML = new Date().getFullYear());
+
+// ----nav toggle----
+const navBtn = document.getElementById("nav-toggle");
+const links = document.getElementById("nav-links");
+navBtn.addEventListener("click", () => {links.classList.toggle("show-links");});
+
+// ----smooth scroll----
+const scrollLinks = document.querySelectorAll(".scroll-link");
+scrollLinks.forEach(link => {link.addEventListener("click", e => {
+   e.preventDefault();
+   links.classList.remove("show-links");
+
+   const id = e.target.getAttribute("href").slice(1);
+   const element = document.getElementById(id);
+   let position = element.offsetTop - 62;
+
+   window.scrollTo({
+      left: 0,
+      // top: element.offsetTop,
+      top: position,
+      behavior: "smooth"
+   })
+})})
